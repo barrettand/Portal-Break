76835b1254e82ca3887f4f56f74a6326f4f7b9d6
LiamDoherty <ldoherty@gmx.com>
2016-05-23 17:27:52 -0400

    Add files via upload

---
+++ PaddleMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleMove : MonoBehaviour {
+
+    // Use this for initialization
+
+    public bool portalActive;
+	void Start () {
+
+        portalActive = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -8.5) {
+            transform.Translate(-0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 8.5)
+        {
+            transform.Translate(0.1f, 0, 0);
+        }
+
+
+    }
+
+   
+}
