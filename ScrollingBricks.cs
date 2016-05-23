a02767a6c860b22b288d94af4c12ea9edc23288a
LiamDoherty <ldoherty@gmx.com>
2016-05-23 16:19:37 -0400

    Add files via upload

---
+++ ScrollingBricks.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBricks : MonoBehaviour {
+
+    Vector3 spawnPosition;
+    float moveIncrement;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 10.0f);
+        moveIncrement = .1f; //Controls speed of movement
+        spawnPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveBrick();
+	}
+
+    void moveBrick()
+    {
+        float positionX = transform.position.x - moveIncrement;
+        Vector3 newPosition = new Vector3(positionX, transform.position.y, transform.position.z);
+        transform.position = newPosition;
+    }
+}
---
+++ SpawnBricks.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnBricks : MonoBehaviour {
+
+
+    [SerializeField] GameObject BrickPrefab;
+    [SerializeField] GameObject SpawnArea;
+
+    ArrayList line1,line2,line3;
+    float spawnAreaHeight;
+    bool canSpawn;
+    float numBricksHeight=3;
+    Vector3 spawnPosition;
+    Vector3 lastBrickPosition;
+    float spawnAreaTopPosY,spawnAreaBotPosY;
+   
+    //Have a spawn area with 3 lines that spawn bricks
+    //3 Arrays
+
+    // Use this for initialization
+    void Start ()
+    {
+        spawnAreaTopPosY = SpawnArea.transform.position.y + (SpawnArea.transform.localScale.y / 2)-BrickPrefab.transform.localScale.y/2;
+        spawnAreaBotPosY = SpawnArea.transform.position.y - (SpawnArea.transform.localScale.y / 2)+BrickPrefab.transform.localScale.y/2;
+        spawnPosition = SpawnArea.transform.position;
+        spawnPosition.y += SpawnArea.gameObject.transform.localScale.y/2 - BrickPrefab.transform.localScale.y / 2; 
+        spawnAreaHeight = SpawnArea.gameObject.transform.localScale.y / numBricksHeight;
+        lastBrickPosition = spawnPosition;
+        canSpawn = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        spawnBrickTimeManger();
+	}
+
+    void spawnBrickTimeManger()
+    {
+        if (canSpawn)
+        {
+            float randomTime = Random.Range(.2f, .3f);
+            Invoke("spawnBrick",randomTime);
+            canSpawn = false;
+        }
+    }
+
+    void spawnBrick()//spawns each brick 
+    {
+        setBrickPositions();
+
+        canSpawn = true;
+        spawnPosition = SpawnArea.transform.position;
+        spawnPosition.y += SpawnArea.gameObject.transform.localScale.y/2 - BrickPrefab.transform.localScale.y / 2;
+        spawnAreaHeight = SpawnArea.gameObject.transform.localScale.y / numBricksHeight;
+
+      
+            float yPosition = Random.Range(spawnAreaBotPosY, spawnAreaTopPosY);          
+           spawnPosition = new Vector3(spawnPosition.x, yPosition, spawnPosition.z);
+               
+       
+        
+    }
+
+    void setBrickPositions()
+    {
+        float interval = spawnAreaHeight / numBricksHeight;
+        Instantiate(BrickPrefab, spawnPosition, Quaternion.identity);
+    }
+}
