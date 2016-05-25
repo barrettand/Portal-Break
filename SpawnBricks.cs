213116c2c72f25ec4d379a4106504d4266b7e296
LiamDoherty <ldoherty@gmx.com>
2016-05-25 12:11:39 -0400

    Updated SpawnBricks Script

--- SpawnBricks.cs
+++ SpawnBricks.cs
@@ -1,72 +1,120 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class SpawnBricks : MonoBehaviour {
+public class SpawnBricks : MonoBehaviour
+{
 
 
-    [SerializeField] GameObject BrickPrefab;
-    [SerializeField] GameObject SpawnArea;
+    [SerializeField]
+    GameObject BrickPrefab;
+    [SerializeField]
+    GameObject SpawnArea;
+    [SerializeField]
+    GameObject RocketPrefab;
+    [SerializeField]
+    GameObject KABOOMPrefab;
+    [SerializeField]
+    GameObject LongBrickPrefab;
+    [SerializeField]
+    GameObject bombCluster;
 
-    ArrayList line1,line2,line3;
-    float spawnAreaHeight;
+    ArrayList line1, line2, line3;
     bool canSpawn;
-    float numBricksHeight=3;
     Vector3 spawnPosition;
-    Vector3 lastBrickPosition;
-    float spawnAreaTopPosY,spawnAreaBotPosY;
-   
+    float spawnAreaTopPosY, spawnAreaBotPosY;
+
+    ArrayList variableBricks;
+    bool longWaitBool;
+    float longWait;
     //Have a spawn area with 3 lines that spawn bricks
     //3 Arrays
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-        spawnAreaTopPosY = SpawnArea.transform.position.y + (SpawnArea.transform.localScale.y / 2)-BrickPrefab.transform.localScale.y/2;
-        spawnAreaBotPosY = SpawnArea.transform.position.y - (SpawnArea.transform.localScale.y / 2)+BrickPrefab.transform.localScale.y/2;
+        spawnAreaTopPosY = SpawnArea.transform.position.y + (SpawnArea.transform.localScale.y / 2) - BrickPrefab.transform.localScale.y / 2;
+        spawnAreaBotPosY = SpawnArea.transform.position.y - (SpawnArea.transform.localScale.y / 2) + BrickPrefab.transform.localScale.y / 2;
         spawnPosition = SpawnArea.transform.position;
-        spawnPosition.y += SpawnArea.gameObject.transform.localScale.y/2 - BrickPrefab.transform.localScale.y / 2; 
-        spawnAreaHeight = SpawnArea.gameObject.transform.localScale.y / numBricksHeight;
-        lastBrickPosition = spawnPosition;
+        spawnPosition.y += SpawnArea.gameObject.transform.localScale.y / 2 - BrickPrefab.transform.localScale.y / 2;
         canSpawn = true;
 
+        //Add Different Bricks here
+        variableBricks = new ArrayList();
+        variableBricks.Add(BrickPrefab);
+        variableBricks.Add(LongBrickPrefab);
+        variableBricks.Add(bombCluster);
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         spawnBrickTimeManger();
-	}
+    }
 
     void spawnBrickTimeManger()
     {
         if (canSpawn)
         {
-            float randomTime = Random.Range(.2f, .3f);
-            Invoke("spawnBrick",randomTime);
+            float randomTime;
+            if (!longWaitBool)
+            {
+                randomTime = Random.Range(3f, 4f);
+            }
+            else
+            {
+                randomTime = 6f;
+            }
+            Invoke("spawnBrick", randomTime);
             canSpawn = false;
         }
     }
 
     void spawnBrick()//spawns each brick 
     {
+        longWaitBool = false;
         setBrickPositions();
-
         canSpawn = true;
         spawnPosition = SpawnArea.transform.position;
-        spawnPosition.y += SpawnArea.gameObject.transform.localScale.y/2 - BrickPrefab.transform.localScale.y / 2;
-        spawnAreaHeight = SpawnArea.gameObject.transform.localScale.y / numBricksHeight;
+        spawnPosition.y += SpawnArea.gameObject.transform.localScale.y / 2 - (BrickPrefab.transform.localScale.y / 2)*2;
 
-      
-            float yPosition = Random.Range(spawnAreaBotPosY, spawnAreaTopPosY);          
-           spawnPosition = new Vector3(spawnPosition.x, yPosition, spawnPosition.z);
-               
-       
-        
+
+        float yPosition = Random.Range(spawnAreaBotPosY, spawnAreaTopPosY);
+        spawnPosition = new Vector3(spawnPosition.x, yPosition, spawnPosition.z);
+
+
+
     }
 
     void setBrickPositions()
     {
-        float interval = spawnAreaHeight / numBricksHeight;
-        Instantiate(BrickPrefab, spawnPosition, Quaternion.identity);
+        float thing = Random.value;
+        int brickSelection=Random.Range(0,variableBricks.Count);
+
+        GameObject currentBrick = (GameObject)variableBricks[brickSelection];
+
+        if(currentBrick.name==LongBrickPrefab.name)
+        {
+            longWaitBool = true;
+
+        }
+
+        if(currentBrick.name==bombCluster.name)
+        {
+            spawnPosition = SpawnArea.transform.position;
+            spawnPosition.y += SpawnArea.gameObject.transform.localScale.y / 2 - (BrickPrefab.transform.localScale.y / 2)*5;
+        }
+
+
+        if (thing <= 0.33)
+        {
+            Instantiate((GameObject)variableBricks[brickSelection], spawnPosition, Quaternion.identity);
+        }
+        if (thing > 0.33 && thing < 0.66)
+        {
+            Instantiate((GameObject)variableBricks[brickSelection], spawnPosition, Quaternion.identity);
+        }
+        else {
+            Instantiate(KABOOMPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 }
